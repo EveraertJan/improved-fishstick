81f2864dc58b97b2e929b981b02e4c51b9fe6d99
Jan Everaert <jan@crashlab.be>
2025-12-23 23:58:27 +0100

    adding web

--- images/api/src/db/knexfile.js
+++ images/api/src/db/knexfile.js
@@ -15,6 +15,16 @@ module.exports = {
       database : process.env.POSTGRES_DATABASE ? process.env.POSTGRES_DATABASE : "postgres"
     }
   },
+   production: {
+    client: 'pg',
+    connection: {
+      host : process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : "localhost",
+      port : 5432,
+      user : process.env.POSTGRES_USER ? process.env.POSTGRES_USER : "admin",
+      password : process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : "test",
+      database : process.env.POSTGRES_DATABASE ? process.env.POSTGRES_DATABASE : "postgres"
+    }
+  },
 
 
 };
